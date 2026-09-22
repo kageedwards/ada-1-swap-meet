6be6cec47613385030a7738614cad1fb5f99ce0b
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-22 14:26:34 -0700

    Implement Clothing subclass of Item

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from .item import Item
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+
+    def __str__(self):
+        common_string = super().__str__()
+        return f"{common_string} It is made from {self.fabric} fabric."
