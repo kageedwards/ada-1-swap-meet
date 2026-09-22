e6b14eb0537ddcf1a5d0351c36772e5d854e2e48
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-22 14:13:58 -0700

    Create Item class, Wave 2 tests passing

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,38 @@
+import uuid
 class Item:
-    pass
+    ## Optional future ID integrity storage
+    ## beyond requirements and not
+    ## compatible with current test suite
+    # valid_id_store = set()
+
+    def check_id_validity(self, new_id):
+        return (
+            ## Is an integer
+            isinstance(new_id, int)
+            ## is a positive number
+            and new_id > 0
+            ## is unique in our global valid_id_store
+            # and new_id not in self.valid_id_store
+        )
+
+    def __init__(self, id=None):
+        id_candidate = id
+
+        while True:
+            # Current candidate is unique enough
+            if self.check_id_validity(id_candidate):
+                break
+
+            # Generate an ID from a new UUID
+            new_uuid = uuid.uuid4()
+            id_candidate = int(new_uuid)
+
+        # Return the final tested ID (int)
+        self.id = id_candidate
+        # self.valid_id_store.add(id_candidate)
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
+
+    def get_category(self):
+        return "Item"
