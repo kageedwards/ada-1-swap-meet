92813bc2132d78246832e77c5d1d49e68af7f301
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-22 14:30:38 -0700

    Implement Electronics subclass of Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,12 @@
-class Electronics:
-    pass
+from .item import Item
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+
+    def get_category(self):
+        return "Electronics"
+
+    def __str__(self):
+        common_string = super().__str__()
+        return f"{common_string} This is a {self.type} device."
