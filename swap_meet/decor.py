7395c8dfe70a1c90ff011b192034230569c57c48
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-22 14:27:01 -0700

    Implement Decor subclass of Item

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,13 @@
-class Decor:
-    pass
+from .item import Item
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+
+    def get_category(self):
+        return "Decor"
+
+    def __str__(self):
+        common_string = super().__str__()
+        return f"{common_string} It takes up a {self.width} by {self.length} sized space."
