2ed0336f87896c73884e525cc58c69cf104dd8c6
Cady <mcmerkle@gmail.com>
2026-09-22 13:23:35 -0700

    Adding get_by_id method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -8,11 +8,21 @@ class Vendor:
             self.inventory = inventory
 
     def add(self, item):
+        """Accepts param item, appends to inventory, returns item"""
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+        """Removes an item from inventory (if present) and returns item, else retuns None"""
         if item in self.inventory:
             self.inventory.remove(item)
             return item
+        return None
+
+    def get_by_id(self, id):
+        """Returns item from inventory with matching id, else None"""
+        for item in self.inventory:
+            if item.id == id:
+                return item
+            
         return None
